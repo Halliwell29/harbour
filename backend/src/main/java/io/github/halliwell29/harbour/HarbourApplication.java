2dda91b7df79a328fa99b0f3413f8d422ef3d59c
Matthew Halliwell <matthew@autough.com>
2026-09-11 16:39:33 -0300

    Initialize Spring Boot backend

---
+++ backend/src/main/java/io/github/halliwell29/harbour/HarbourApplication.java
@@ -0,0 +1,13 @@
+package io.github.halliwell29.harbour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarbourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarbourApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/io/github/halliwell29/harbour/HarbourApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.halliwell29.harbour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarbourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
