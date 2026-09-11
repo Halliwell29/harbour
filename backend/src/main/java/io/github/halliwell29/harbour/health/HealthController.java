dbaa5df2aee04839461567991d1da4d6b146fc99
Matthew Halliwell <matthew@autough.com>
2026-09-11 17:58:28 -0300

    Read service name from configuration

--- backend/src/main/java/io/github/halliwell29/harbour/health/HealthController.java
+++ backend/src/main/java/io/github/halliwell29/harbour/health/HealthController.java
@@ -1,5 +1,6 @@
 package io.github.halliwell29.harbour.health;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,10 +8,15 @@ import java.time.Instant;
 
 @RestController
 public class HealthController {
+    private final String serviceName;
+
+    public HealthController(@Value("${spring.application.name}") String serviceName) {
+        this.serviceName = serviceName;
+    }
 
     @GetMapping("/health")
     public HealthResponse getHealth() {
-        return new HealthResponse("UP", "harbour", Instant.now());
+        return new HealthResponse("UP", serviceName, Instant.now());
     }
 
 }
