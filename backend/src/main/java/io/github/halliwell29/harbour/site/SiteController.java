69702c1d731cec49e41938e9c12b37c56b5a7b16
Matthew Halliwell <matthew@autough.com>
2026-09-14 18:21:40 -0300

    feat: implement site DTO, service and controller

--- backend/src/main/java/io/github/halliwell29/harbour/site/SiteController.java
+++ backend/src/main/java/io/github/halliwell29/harbour/site/SiteController.java
@@ -1,4 +1,27 @@
 package io.github.halliwell29.harbour.site;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/sites")
 public class SiteController {
+
+    private final SiteService siteService;
+
+    public SiteController(SiteService siteService){
+        this.siteService = siteService;
+    }
+
+    @GetMapping
+    public List<SiteResponse> getSites() {
+        return siteService.findAll()
+                .stream()
+                .map(SiteResponse::from)
+                .toList();
+    }
+
 }
--- backend/src/main/java/io/github/halliwell29/harbour/site/SiteService.java
+++ backend/src/main/java/io/github/halliwell29/harbour/site/SiteService.java
@@ -1,4 +1,18 @@
 package io.github.halliwell29.harbour.site;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SiteService {
+    private final SiteRepository siteRepository;
+
+    public SiteService(SiteRepository siteRepository){
+        this.siteRepository = siteRepository;
+    }
+
+    public List<Site> findAll(){
+        return siteRepository.findAll();
+    }
 }
